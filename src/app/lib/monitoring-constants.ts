0c7961cfc0d186e5a8aaa5f017c149d50a9738e3
Estudios Ambientales DPH <ambientales.dph@gmail.com>
2026-05-21 14:49:53 +0000

    Ahora veo esto. Porqué, si el campo del json se llama nombre_2 para todo

--- src/app/lib/monitoring-constants.ts
+++ src/app/lib/monitoring-constants.ts
@@ -38,6 +38,9 @@ export const BASIN_CODES_DATA = {
     { "type": "Feature", "properties": { "CODIGO": "SAL", "nombre_2": "Salado" }, "geometry": { "type": "Point", "coordinates": [-59.98, -35.74] } },
     { "type": "Feature", "properties": { "CODIGO": "Luj", "nombre_2": "Lujan" }, "geometry": { "type": "Point", "coordinates": [-58.92, -34.45] } },
     { "type": "Feature", "properties": { "CODIGO": "REC", "nombre_2": "Reconquista" }, "geometry": { "type": "Point", "coordinates": [-58.62, -34.48] } },
-    { "type": "Feature", "properties": { "CODIGO": "RPM", "nombre_2": "Vertiente Río de La Plata Intermedia" }, "geometry": { "type": "Point", "coordinates": [-58.00, -34.70] } }
+    { "type": "Feature", "properties": { "CODIGO": "RPM", "nombre_2": "Vertiente Río de La Plata Intermedia" }, "geometry": { "type": "Point", "coordinates": [-58.00, -34.70] } },
+    { "type": "Feature", "properties": { "CODIGO": "RSA", "nombre_2": "Vertiente del Río Samborombón Autóctona" }, "geometry": { "type": "Point", "coordinates": [-58.30, -35.20] } },
+    { "type": "Feature", "properties": { "CODIGO": "VAS", "nombre_2": "Vertiente del Río de la Plata Superior" }, "geometry": { "type": "Point", "coordinates": [-58.50, -34.45] } },
+    { "type": "Feature", "properties": { "CODIGO": "VASE", "nombre_2": "Vertiente del Río de la Plata Superior (Extremo)" }, "geometry": { "type": "Point", "coordinates": [-58.60, -34.30] } }
   ]
 };
